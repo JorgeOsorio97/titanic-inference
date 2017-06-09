e9543c8bfb48a57de042afb8e05d77e94c83de95
JorgeOsorio97 <jorge.valdez.osorio@gmail.com>
2017-06-09 11:28:52 +0800

    Practicas con plt.bar()

--- titanic.py
+++ titanic.py
@@ -1,10 +1,18 @@
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
 
-train_df = pd.read_csv("kaggle/titanic/train.csv")
+
+train_df = pd.read_csv("kaggle/titanic-inference//train.csv")
 isMale = train_df["Sex"] == 'male'
 males = train_df[isMale]
 boolLifeMales = males['Survived']==1
-print(boolLifeMales)
+#print(boolLifeMales)
 lifeMales = males[boolLifeMales]
-print(lifeMales)
+count_lifeMales = len(lifeMales['PassengerId'])
+#print(lifeMales)
+
+plt.xticks('Men','Women')
+plt.bar(2, count_lifeMales)
+plt.show()

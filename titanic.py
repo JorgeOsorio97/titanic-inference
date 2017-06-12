ad68dc7aeeea8333b234a397f9eae3a505c1add1
JorgeOsorio97 <jorge.valdez.osorio@gmail.com>
2017-06-11 20:09:42 -0500

    alta en domingo no info

--- titanic.py
+++ titanic.py
@@ -1,18 +1,44 @@
+#import requires libraries
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+#import Data
+train_df = pd.read_csv("kaggle/titanic-inference/train.csv")
+test_df = pd.read_csv("kaggle/titanic-inference/test.csv")
 
-
-train_df = pd.read_csv("kaggle/titanic-inference//train.csv")
+#Defining if being male is and important variable for survival
 isMale = train_df["Sex"] == 'male'
 males = train_df[isMale]
 boolLifeMales = males['Survived']==1
-#print(boolLifeMales)
 lifeMales = males[boolLifeMales]
 count_lifeMales = len(lifeMales['PassengerId'])
-#print(lifeMales)
+count_deadMales = len(males['PassengerId']) - count_lifeMales
+print(count_lifeMales)
+print(count_deadMales)
 
-plt.xticks('Men','Women')
-plt.bar(2, count_lifeMales)
+isFemale = train_df["Sex"] == 'female'
+females = train_df[isFemale]
+boolLifeFemales = females['Survived']==1
+lifeFemales = females[boolLifeFemales]
+count_lifeFemales = len(lifeFemales['PassengerId'])
+count_deadFemales = len(females['PassengerId']) - count_lifeFemales
+print(count_lifeFemales)
+print(count_deadFemales)
+
+#Pie Chart
+labels = ['Life Males', 'Life Females', 'Dead Males', 'Dead Females']
+sizes = [count_lifeMales, count_lifeFemales, count_deadMales, count_deadFemales]
+explode = [0.1,0.1,0.1,0.1]
+plt.title('Percentage of dead or alive males or females')
+plt.pie(sizes, explode=explode, labels= labels, startangle=90, shadow=True, autopct='%1.1f%%')
+"""
+#Bar Chart
+N = 4
+ind = np.arange(N)
+plt.bar(ind, sizes, width = 0.8)
+"""
+
 plt.show()
+
+
